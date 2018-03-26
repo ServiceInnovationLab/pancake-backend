05ebd21e8737dc96e6fb99af90bd9b679c3a94c2
Brenda Wallace <shiny@cpan.org>
2018-03-26 13:26:51 +1300

    Scopes for rebate form signatures

--- app/models/rebate_form.rb
+++ app/models/rebate_form.rb
@@ -2,4 +2,12 @@
 
 class RebateForm < ApplicationRecord
   has_many :signatures, dependent: :destroy
+
+  def applicant_signature
+    signatures.applicant.first
+  end
+
+  def witness_signature
+    signatures.witness.first
+  end
 end
