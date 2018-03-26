d5d6d97c00b43671bbce0cf482bc95bd84c8e18d
Brenda Wallace <shiny@cpan.org>
2018-03-26 13:22:18 +1300

    Signature scopes

--- app/models/signature.rb
+++ app/models/signature.rb
@@ -3,4 +3,7 @@
 class Signature < ApplicationRecord
   belongs_to :rebate_form
   belongs_to :signature_type
+
+  scope :applicant, -> { joins(:signature_type).where("signature_types.name": 'applicant') }
+  scope :witness, -> { joins(:signature_type).where("signature_types.name": 'witness') }
 end
--- spec/models/signature_spec.rb
+++ spec/models/signature_spec.rb
@@ -3,12 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Signature, type: :model do
-  let(:rebate_form) { FactoryBot.create :rebate_form }
-  let(:witness) { FactoryBot.create :witness_signature, rebate_form: rebate_form }
-  let(:applicant) { FactoryBot.create :applicant_signature, rebate_form: rebate_form }
+  let!(:rebate_form) { FactoryBot.create :rebate_form }
+  let!(:witness) { FactoryBot.create :witness_signature, rebate_form: rebate_form }
+  let!(:applicant) { FactoryBot.create :applicant_signature, rebate_form: rebate_form }
 
   describe 'signature type' do
     it { expect(witness.signature_type.name).to eq 'witness' }
     it { expect(applicant.signature_type.name).to eq 'applicant' }
+  end
+
+  describe 'scopes' do
+    it { expect(Signature.applicant).to eq [applicant] }
+    it { expect(Signature.witness).to eq [witness] }
   end
 end
