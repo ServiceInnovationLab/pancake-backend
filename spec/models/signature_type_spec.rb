7712ba3638455314860a3ce4bb7d5a8b6702e836
Brenda Wallace <shiny@cpan.org>
2018-03-26 13:09:53 +1300

    Scopes for signature types

--- spec/models/signature_type_spec.rb
+++ spec/models/signature_type_spec.rb
@@ -3,6 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe SignatureType, type: :model do
-  let(:signature_type) { FactoryBot.create(:signature_type, name: 'witness') }
-  it { expect(signature_type.name).to eq 'witness' }
+  let!(:applicant_type) { FactoryBot.create(:signature_type, name: 'applicant') }
+  let!(:witness_type) { FactoryBot.create(:signature_type, name: 'witness') }
+  it { expect(witness_type.name).to eq 'witness' }
+  describe 'scopes' do
+    it { expect(SignatureType.applicant).to eq [applicant_type] }
+    it { expect(SignatureType.witness).to eq [witness_type] }
+  end
 end
