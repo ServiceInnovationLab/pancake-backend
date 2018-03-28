eda6b442e46fe61f3038ded760c4fdadced6194b
Brenda Wallace <shiny@cpan.org>
2018-03-28 23:31:42 +1300

    Checks you can't look up forms by id

--- spec/api/v1/rebate_forms/show_spec.rb
+++ spec/api/v1/rebate_forms/show_spec.rb
@@ -5,17 +5,30 @@ require 'rails_helper'
 RSpec.describe 'rebate_forms#show', type: :request do
   let(:params) { {} }
 
-  subject(:make_request) do
-    jsonapi_get "/api/v1/rebate_forms/#{rebate_form.id}",
-                params: params
-  end
+  let!(:rebate_form) { FactoryBot.create(:rebate_form) }
 
-  describe 'basic fetch' do
-    let!(:rebate_form) { create(:rebate_form) }
+  describe 'fetch by token' do
+    subject(:make_request) do
+      jsonapi_get "/api/v1/rebate_forms/#{rebate_form.token}",
+                  params: params
+    end
 
-    pending 'serializes the resource correctly' do
+    it 'retrieves form by token' do
       make_request
       assert_payload(:rebate_form, rebate_form, json_item)
+    end
+  end
+
+  describe 'fetch by id' do
+    subject(:make_request) do
+      jsonapi_get "/api/v1/rebate_forms/#{rebate_form.id}",
+                  params: params
+    end
+
+    it 'does not retrieve form by id' do
+      expect do
+        make_request
+      end.to raise_error(JsonapiCompliable::Errors::RecordNotFound)
     end
   end
 end
