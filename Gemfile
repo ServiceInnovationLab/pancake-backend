a0b0fa28865e5317e6034c83ba7b37293f4c80b9
Brenda Wallace <shiny@cpan.org>
2018-04-03 13:18:37 +1200

    Devise added

--- Gemfile
+++ Gemfile
@@ -7,10 +7,9 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
 
-# Use sqlite3 as the database for Active Record
+# Database
 gem 'pg'
 
 # Use Puma as the app server
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321015659) do
+ActiveRecord::Schema.define(version: 20180329012945) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +37,30 @@ ActiveRecord::Schema.define(version: 20180321015659) do
     t.datetime "updated_at", null: false
     t.index ["rebate_form_id"], name: "index_signatures_on_rebate_form_id"
     t.index ["signature_type_id"], name: "index_signatures_on_signature_type_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "signatures", "rebate_forms"
