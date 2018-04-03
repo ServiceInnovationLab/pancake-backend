10fb444fcecdc017e1a1ad744141b05505db35e1
Brenda Wallace <shiny@cpan.org>
2018-04-03 16:10:19 +1200

    Remove commented out code

--- config/application.rb
+++ config/application.rb
@@ -36,22 +36,3 @@ module PancakeServer
     config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
-
-# require_relative 'boot'
-
-# require 'rails/all'
-
-# # Require the gems listed in Gemfile, including any gems
-# # you've limited to :test, :development, or :production.
-# Bundler.require(*Rails.groups)
-
-# module PancakeServer
-#   class Application < Rails::Application
-#     # Initialize configuration defaults for originally generated Rails version.
-#     config.load_defaults 5.1
-
-#     # Settings in config/environments/* take precedence over those specified here.
-#     # Application configuration should go into files in config/initializers
-#     # -- all .rb files in that directory are automatically loaded.
-#   end
-# end
