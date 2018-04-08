6c17d7d9856c9195b8dbd45775b5a6af076d80cf
Awesome Code <team@xinminlabs.com>
2018-04-08 22:15:41 +0000

    Auto corrected by following spaced-comment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // About supported directives.
 //
-//= require rails-ujs
-//= require_tree .
+// = require rails-ujs
+// = require_tree .
