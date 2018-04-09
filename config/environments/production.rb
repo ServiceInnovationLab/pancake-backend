8c34580c776f6036ae58354871c2333fa1c6250b
Brenda Wallace <shiny@cpan.org>
2018-04-09 15:22:13 +1200

    Add hostname for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+  config.action_mailer.default_url_options = { host: ENV['HOSTNAME'] }
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
