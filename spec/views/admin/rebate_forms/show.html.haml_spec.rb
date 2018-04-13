0d181ef3744dccefd7a60db0aa08e8516b6c8049
Brenda Wallace <shiny@cpan.org>
2018-04-13 14:21:40 +1200

    Update view specs, for rates_rebate - adidng required values

--- spec/views/admin/rebate_forms/show.html.haml_spec.rb
+++ spec/views/admin/rebate_forms/show.html.haml_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'admin/rebate_forms/show', type: :view do
   before do
-    @admin_rebate_form = assign(:rebate_form, RebateForm.create!)
+    @admin_rebate_form = assign(:rebate_form, FactoryBot.create(:rebate_form))
   end
 
   it 'renders attributes in <p>' do
