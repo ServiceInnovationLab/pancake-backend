692cfc9a426e386b22b83db4b4c8a50d5bf06137
Brenda Wallace <shiny@cpan.org>
2018-04-13 14:33:24 +1200

    Fixed path in new form spec

--- spec/views/admin/rebate_forms/new.html.haml_spec.rb
+++ spec/views/admin/rebate_forms/new.html.haml_spec.rb
@@ -3,15 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe 'admin/rebate_forms/new', type: :view do
-  let!(:rebate_form) { FactoryBot.create(:rebate_form) }
+  let(:rebate_form) { FactoryBot.create(:rebate_form) }
   before do
     assign(:rebate_form, rebate_form)
   end
 
   it 'renders new admin_rebate_form form' do
     render
-
-    assert_select 'form[action=?][method=?]', admin_rebate_forms_path, 'post' do
+    assert_select 'form[action=?][method=?]', admin_rebate_form_path(rebate_form), 'post' do
     end
   end
 end
