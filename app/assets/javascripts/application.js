dd7671280b250a44259d4bdad1afb1fa93946dee
Awesome Code <team@xinminlabs.com>
2018-04-16 23:37:39 +0000

    Auto corrected by following multiline-comment-style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,16 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// Listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// Vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// Compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// About supported directives.
-//
-// = require rails-ujs
-// = require_tree .
+/*
+ * This is a manifest file that'll be compiled into application.js, which will include all the files
+ * Listed below.
+ *
+ * Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+ * Vendor/assets/javascripts directory can be referenced here using a relative path.
+ *
+ * It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+ * Compiled file. JavaScript code in this file should be added after the last require_* statement.
+ *
+ * Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+ * About supported directives.
+ *
+ * = require rails-ujs
+ * = require_tree .
+ */
