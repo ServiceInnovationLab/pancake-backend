6ce6de5737ef104be0ded0ffd2e3d44e2364ad8f
Awesome Code <team@xinminlabs.com>
2018-04-16 23:41:12 +0000

    Auto corrected by following GeneralLayout

--- spec/views/admin/rebate_forms/index.html.haml_spec.rb
+++ spec/views/admin/rebate_forms/index.html.haml_spec.rb
@@ -4,10 +4,12 @@ require 'rails_helper'
 
 RSpec.describe 'admin/rebate_forms/index', type: :view do
   before do
-    assign(:rebate_forms, [
-             FactoryBot.create(:rebate_form),
-             FactoryBot.create(:rebate_form)
-           ])
+    assign(
+      :rebate_forms, [
+        FactoryBot.create(:rebate_form),
+        FactoryBot.create(:rebate_form)
+      ]
+    )
   end
 
   it 'renders a list of admin/rebate_forms' do
