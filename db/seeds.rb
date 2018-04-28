93c6de9ac2cc8b1d52013724101603136527abd9
Brenda Wallace <shiny@cpan.org>
2018-04-28 12:31:14 +1200

    Names and roles on signatures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,8 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+SignatureType.find_or_create_by! name: 'applicant'
+SignatureType.find_or_create_by! name: 'witness'
 
-SignatureType.create! name: 'applicant'
-SignatureType.create! name: 'witness'
 
 User.invite! email: 'brenda.wallace@dia.govt.nz'
 User.invite! email: 'dana.iti@dia.govt.nz'
