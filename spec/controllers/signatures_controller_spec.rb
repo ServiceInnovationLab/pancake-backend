95200a4ea8b10b7fb5839ce82345a918468c0f2b
Awesome Code <team@xinminlabs.com>
2018-05-01 01:53:29 +0000

    Auto corrected by following RSpec/EmptyLineAfterSubject

--- spec/controllers/signatures_controller_spec.rb
+++ spec/controllers/signatures_controller_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 RSpec.describe SignaturesController, type: :controller do
   subject { JSON.parse(response.body)['data']['attributes'] }
+
   let(:rebate_form) { FactoryBot.create :rebate_form }
   let(:signature_type) { FactoryBot.create :signature_type, name: 'applicant' }
 
