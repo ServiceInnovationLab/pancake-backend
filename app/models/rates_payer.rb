a04b712d937c21410c9d99f4954bc97ed328e77e
Brenda Wallace <shiny@cpan.org>
2018-05-01 12:25:05 +1200

    RatesPayer and RatesBill must have a property

--- app/models/rates_payer.rb
+++ app/models/rates_payer.rb
@@ -4,5 +4,5 @@ class RatesPayer < ApplicationRecord
   # For this app, we don't need to show that a
   # rate payer may have maybe properties.
   # We only care about the one they lived in.
-  belongs_to :property
+  belongs_to :property, required: true
 end
