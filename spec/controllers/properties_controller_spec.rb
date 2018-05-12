1ba2dfcabee1bcc45442dfb0dfb0412ad79e40e8
Brenda Wallace <shiny@cpan.org>
2018-05-12 15:31:22 +1200

    rubocop fixes

--- spec/controllers/properties_controller_spec.rb
+++ spec/controllers/properties_controller_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 
 RSpec.describe PropertiesController, type: :controller do
   describe '#index' do
-    let!(:property_1) { FactoryBot.create :property, location: "11 MAIN ROAD" }
-    let!(:property_2) { FactoryBot.create :property, location: "11 MOANA ROAD" }
+    let!(:property_1) { FactoryBot.create :property, location: '11 MAIN ROAD' }
+    let!(:property_2) { FactoryBot.create :property, location: '11 MOANA ROAD' }
 
     subject { JSON.parse(response.body)['data'] }
     before { get :index, format: :json, params: { q: 'moana' } }
@@ -21,15 +21,14 @@ RSpec.describe PropertiesController, type: :controller do
 
     it do
       is_expected.to eq(
-      {"id"=>property_1.valuation_id,
-        "type"=>"properties",
-        "attributes"=>
-         {"valuation_id"=> property_1.valuation_id,
-          "location"=>"123 Lambton Quay",
-          "suburb"=>"Te Aro",
-          "town_city"=>"Poneke"},
-        "relationships" => {"rates_payers"=>{"data"=>[]}, "rates_bills"=>{"data"=>[]}}
-        }
+        'id' => property_1.valuation_id,
+        'type' => 'properties',
+        'attributes' =>
+          { 'valuation_id' => property_1.valuation_id,
+            'location' => '123 Lambton Quay',
+            'suburb' => 'Te Aro',
+            'town_city' => 'Poneke' },
+        'relationships' => { 'rates_payers' => { 'data' => [] }, 'rates_bills' => { 'data' => [] } }
       )
     end
   end
