9d7e76f511052527f474f643a7572a880aca9ed8
Brenda Wallace <shiny@cpan.org>
2018-05-14 15:01:29 +1200

    Pull the frontend into the server

---
+++ bin/frontend-update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euv
+
+APP_NAME="pancake-frontend"
+BRANCH="feature/deployment"
+
+git clone https://github.com/ServiceInnovationLab/$APP_NAME.git --branch $BRANCH
+cd $APP_NAME
+
+npm install
+npm run build
+
+rm -r public/app
+mv build ../public/app
+
+cd ..
+rm -r $APP_NAME
