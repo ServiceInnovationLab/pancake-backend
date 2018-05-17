ec905ff17dadbeda0a16946c03091c0e982e53be
Brenda Wallace <shiny@cpan.org>
2018-05-17 16:03:38 +1200

    rubocop fixup

--- spec/models/rates_bill_spec.rb
+++ spec/models/rates_bill_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe RatesBill, type: :model do
   describe 'calculates entire bill' do
     let(:property) { FactoryBot.create :property }
     let(:bill) { FactoryBot.create :rates_bill, total_rates: 100, total_water_rates: 10.4, property: property }
-    it { expect(bill.total_bill).to eq "110.40" }
+    it { expect(bill.total_bill).to eq '110.40' }
   end
 end
