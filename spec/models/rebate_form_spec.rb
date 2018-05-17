3ccdf87377c0a5ed98a7901cfca044d19e6c52a7
Brenda Wallace <shiny@cpan.org>
2018-05-17 21:42:34 +1200

    Validation specs

--- spec/models/rebate_form_spec.rb
+++ spec/models/rebate_form_spec.rb
@@ -46,8 +46,8 @@ RSpec.describe RebateForm, type: :model do
   describe 'calculates rebate' do
     let!(:rates_bill) { FactoryBot.create :rates_bill, total_rates: 3450, total_water_rates: 5, property: property, rating_year: ENV['YEAR'] }
     let(:form) { FactoryBot.create :rebate_form, property: property, valuation_id: valuation_id, fields: fields }
-    let(:fields) { {"income": 39900.00, "dependants": 1, "full_name": "Edith" }}
-    before{ form.calc_rebate_amount! }
+    let(:fields) { { "income": 39_900.00, "dependants": 1, "full_name": 'Edith' } }
+    before { form.calc_rebate_amount! }
     it { expect(form.rebate).to eq 370.42 }
   end
 end
