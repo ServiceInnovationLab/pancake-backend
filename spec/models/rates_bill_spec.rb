d6e6756014ae1555845342252d521115ff2076c5
Brenda Wallace <shiny@cpan.org>
2018-05-17 15:58:53 +1200

    Spec to test bill is calculated

--- spec/models/rates_bill_spec.rb
+++ spec/models/rates_bill_spec.rb
@@ -3,5 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe RatesBill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'calculates entire bill' do
+    let(:property) { FactoryBot.create :property }
+    let(:bill) { FactoryBot.create :rates_bill, total_rates: 100, total_water_rates: 10.4, property: property }
+    it { expect(bill.total_bill).to eq "110.40" }
+  end
 end
