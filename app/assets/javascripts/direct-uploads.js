d3d02c1b3f774fe91a552b55121820727b20e903
Brenda Wallace <shiny@cpan.org>
2018-05-22 15:18:20 +1200

    Added semicolons to js

--- app/assets/javascripts/direct-uploads.js
+++ app/assets/javascripts/direct-uploads.js
@@ -1,12 +1,12 @@
 addEventListener("direct-upload:initialize", event => {
-  const { target, detail } = event
-  const { id, file } = detail
+  const { target, detail } = event;
+  const { id, file } = detail;
   target.insertAdjacentHTML("beforebegin", `
     <div id="direct-upload-${id}" class="direct-upload direct-upload--pending">
       <div id="direct-upload-progress-${id}" class="direct-upload__progress" style="width: 0%"></div>
       <span class="direct-upload__filename">${file.name}</span>
     </div>
-  `)
+  `);
 });
 
 addEventListener("direct-upload:start", event => {
