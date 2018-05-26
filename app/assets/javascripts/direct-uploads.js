d26c90e0e62ba6236ac7128e47717469af91484e
Awesome Code <team@xinminlabs.com>
2018-05-26 09:20:48 +0000

    Auto corrected by following object-curly-spacing

--- app/assets/javascripts/direct-uploads.js
+++ app/assets/javascripts/direct-uploads.js
@@ -1,6 +1,6 @@
 addEventListener('direct-upload:initialize', event => {
-  const { target, detail } = event;
-  const { id, file } = detail;
+  const {target, detail} = event;
+  const {id, file} = detail;
   target.insertAdjacentHTML('beforebegin', `
     <div id="direct-upload-${id}" class="direct-upload direct-upload--pending">
       <div id="direct-upload-progress-${id}" class="direct-upload__progress" style="width: 0%"></div>
@@ -10,27 +10,27 @@ addEventListener('direct-upload:initialize', event => {
 });
 
 addEventListener('direct-upload:start', event => {
-  const { id } = event.detail;
+  const {id} = event.detail;
   const element = document.getElementById(`direct-upload-${id}`);
   element.classList.remove('direct-upload--pending');
 });
 
 addEventListener('direct-upload:progress', event => {
-  const { id, progress } = event.detail;
+  const {id, progress} = event.detail;
   const progressElement = document.getElementById(`direct-upload-progress-${id}`);
   progressElement.style.width = `${progress}%`;
 });
 
 addEventListener('direct-upload:error', event => {
   event.preventDefault();
-  const { id, error } = event.detail;
+  const {id, error} = event.detail;
   const element = document.getElementById(`direct-upload-${id}`);
   element.classList.add('direct-upload--error');
   element.setAttribute('title', error);
 });
 
 addEventListener('direct-upload:end', event => {
-  const { id } = event.detail;
+  const {id} = event.detail;
   const element = document.getElementById(`direct-upload-${id}`);
   element.classList.add('direct-upload--complete');
 });
