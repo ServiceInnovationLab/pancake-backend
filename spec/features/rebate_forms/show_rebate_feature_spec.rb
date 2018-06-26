4f18b5d3da9242db217603997efc4fe8073a3de7
Awesome Code <team@xinminlabs.com>
2018-06-26 03:32:41 +0000

    Auto corrected by following Capybara/FeatureMethods

--- spec/features/rebate_forms/show_rebate_feature_spec.rb
+++ spec/features/rebate_forms/show_rebate_feature_spec.rb
@@ -2,10 +2,10 @@
 
 require 'rails_helper'
 
-RSpec.feature 'RebateForm', type: :feature do
+RSpec.describe 'RebateForm', type: :feature do
   let(:rebate_form) { FactoryBot.create :rebate_form }
 
-  scenario "Anonymous can't see it" do
+  it "Anonymous can't see it" do
     visit "/admin/rebate_forms/#{rebate_form.id}"
     expect(page).to have_text('Forgot your password?')
   end
