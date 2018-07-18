033089367483a604cd2b7b83fdd8f72b3d239f7b
Brenda Wallace <shiny@cpan.org>
2018-07-18 15:12:44 +1200

    Fixing up request specs

--- spec/requests/admin/admin_councils_spec.rb
+++ spec/requests/admin/admin_councils_spec.rb
@@ -2,11 +2,11 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Admin::RebateForms', type: :request do
+RSpec.describe 'Admin::Councils', type: :request do
   context 'not signed in' do
-    describe 'GET /admin_rebate_forms' do
+    describe 'GET /admin/councils' do
       it 'works! (now write some real specs)' do
-        get admin_rebate_forms_path
+        get admin_councils_path
         expect(response).to have_http_status(302)
       end
     end
