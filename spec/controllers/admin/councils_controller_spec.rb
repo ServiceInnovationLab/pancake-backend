6cb1725dcf71d6258756a0ab5126360d0a3c7620
Brenda Wallace <shiny@cpan.org>
2018-07-20 14:12:54 +1200

    Removing unnecesary stuff from councils controller spec

--- spec/controllers/admin/councils_controller_spec.rb
+++ spec/controllers/admin/councils_controller_spec.rb
@@ -8,9 +8,6 @@ RSpec.describe Admin::CouncilsController, type: :controller do
   context 'signed in as admin' do
     before { sign_in admin_user }
 
-    # This should return the minimal set of attributes required to create a valid
-    # Council. As you add validations to Council, be sure to
-    # adjust the attributes here as well.
     let(:valid_attributes) do
       { name: 'Tauranga', active: true }
     end
@@ -18,16 +15,10 @@ RSpec.describe Admin::CouncilsController, type: :controller do
     let(:invalid_attributes) do
       skip('Add a hash of attributes invalid for your model')
     end
-
-    # This should return the minimal set of values that should be in the session
-    # in order to pass any filters (e.g. authentication) defined in
-    # CouncilsController. Be sure to keep this updated too.
-    let(:valid_session) { {} }
-
     describe 'GET #index' do
       it 'assigns all councils as @councils' do
         council = Council.create! valid_attributes
-        get :index, params: {}, session: valid_session
+        get :index, params: {}
         expect(assigns(:councils)).to eq([council])
       end
     end
@@ -35,14 +26,14 @@ RSpec.describe Admin::CouncilsController, type: :controller do
     describe 'GET #show' do
       it 'assigns the requested council as @council' do
         council = Council.create! valid_attributes
-        get :show, params: { id: council.to_param }, session: valid_session
+        get :show, params: { id: council.to_param }
         expect(assigns(:council)).to eq(council)
       end
     end
 
     describe 'GET #new' do
       it 'assigns a new council as @council' do
-        get :new, params: {}, session: valid_session
+        get :new, params: {}
         expect(assigns(:council)).to be_a_new(Council)
       end
     end
@@ -50,7 +41,7 @@ RSpec.describe Admin::CouncilsController, type: :controller do
     describe 'GET #edit' do
       it 'assigns the requested council as @council' do
         council = Council.create! valid_attributes
-        get :edit, params: { id: council.to_param }, session: valid_session
+        get :edit, params: { id: council.to_param }
         expect(assigns(:council)).to eq(council)
       end
     end
@@ -59,30 +50,30 @@ RSpec.describe Admin::CouncilsController, type: :controller do
       context 'with valid params' do
         it 'creates a new Council' do
           expect do
-            post :create, params: { council: valid_attributes }, session: valid_session
+            post :create, params: { council: valid_attributes }
           end.to change(Council, :count).by(1)
         end
 
         it 'assigns a newly created council as @council' do
-          post :create, params: { council: valid_attributes }, session: valid_session
+          post :create, params: { council: valid_attributes }
           expect(assigns(:council)).to be_a(Council)
           expect(assigns(:council)).to be_persisted
         end
 
         it 'redirects to the created council' do
-          post :create, params: { council: valid_attributes }, session: valid_session
+          post :create, params: { council: valid_attributes }
           expect(response).to redirect_to(admin_councils_path)
         end
       end
 
       context 'with invalid params' do
         it 'assigns a newly created but unsaved council as @council' do
-          post :create, params: { council: invalid_attributes }, session: valid_session
+          post :create, params: { council: invalid_attributes }
           expect(assigns(:council)).to be_a_new(Council)
         end
 
         it "re-renders the 'new' template" do
-          post :create, params: { council: invalid_attributes }, session: valid_session
+          post :create, params: { council: invalid_attributes }
           expect(response).to render_template('new')
         end
       end
@@ -96,20 +87,20 @@ RSpec.describe Admin::CouncilsController, type: :controller do
 
         it 'updates the requested council' do
           council = Council.create! valid_attributes
-          put :update, params: { id: council.to_param, council: new_attributes }, session: valid_session
+          put :update, params: { id: council.to_param, council: new_attributes }
           council.reload
           skip('Add assertions for updated state')
         end
 
         it 'assigns the requested council as @council' do
           council = Council.create! valid_attributes
-          put :update, params: { id: council.to_param, council: valid_attributes }, session: valid_session
+          put :update, params: { id: council.to_param, council: valid_attributes }
           expect(assigns(:council)).to eq(council)
         end
 
         it 'redirects to the council' do
           council = Council.create! valid_attributes
-          put :update, params: { id: council.to_param, council: valid_attributes }, session: valid_session
+          put :update, params: { id: council.to_param, council: valid_attributes }
           expect(response).to redirect_to(admin_councils_path)
         end
       end
@@ -117,13 +108,13 @@ RSpec.describe Admin::CouncilsController, type: :controller do
       context 'with invalid params' do
         it 'assigns the council as @council' do
           council = Council.create! valid_attributes
-          put :update, params: { id: council.to_param, council: invalid_attributes }, session: valid_session
+          put :update, params: { id: council.to_param, council: invalid_attributes }
           expect(assigns(:council)).to eq(council)
         end
 
         it "re-renders the 'edit' template" do
           council = Council.create! valid_attributes
-          put :update, params: { id: council.to_param, council: invalid_attributes }, session: valid_session
+          put :update, params: { id: council.to_param, council: invalid_attributes }
           expect(response).to render_template('edit')
         end
       end
@@ -133,13 +124,13 @@ RSpec.describe Admin::CouncilsController, type: :controller do
       it 'destroys the requested council' do
         council = Council.create! valid_attributes
         expect do
-          delete :destroy, params: { id: council.to_param }, session: valid_session
+          delete :destroy, params: { id: council.to_param }
         end.to change(Council, :count).by(-1)
       end
 
       it 'redirects to the councils list' do
         council = Council.create! valid_attributes
-        delete :destroy, params: { id: council.to_param }, session: valid_session
+        delete :destroy, params: { id: council.to_param }
         expect(response).to redirect_to(admin_councils_url)
       end
     end
