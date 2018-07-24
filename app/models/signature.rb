c9740713fa18d2474a0cff6b589543c322221b35
Brenda Wallace <shiny@cpan.org>
2018-07-25 09:36:03 +1200

    Moved rebate_form.completed update to hooks on signature

--- app/models/signature.rb
+++ app/models/signature.rb
@@ -3,11 +3,16 @@
 class Signature < ApplicationRecord
   belongs_to :rebate_form
   belongs_to :signature_type
-
   validates :signature_type, presence: true
-  # validates :image, presence: true
-  # validates :name, presence: true
 
   scope :applicant, -> { joins(:signature_type).where("signature_types.name": 'applicant') }
   scope :witness, -> { joins(:signature_type).where("signature_types.name": 'witness') }
+
+  after_save :update_form_completed
+  after_destroy :update_form_completed
+
+  def update_form_completed
+    rebate_form.completed = (rebate_form.applicant_signature.present? && rebate_form.witness_signature.present?)
+    rebate_form.save
+  end
 end
