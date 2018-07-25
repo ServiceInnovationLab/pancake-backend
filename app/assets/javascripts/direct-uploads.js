6e7f83b6ba972fadef30af039e42d018380b04d2
Awesome Code <team@xinminlabs.com>
2018-07-25 10:21:55 +0000

    Auto corrected by following Javascript indent

--- app/assets/javascripts/direct-uploads.js
+++ app/assets/javascripts/direct-uploads.js
@@ -1,6 +1,6 @@
 addEventListener('direct-upload:initialize', event => {
   const { target, detail } = event,
-   { id, file } = detail;
+    { id, file } = detail;
   target.insertAdjacentHTML('beforebegin', `
     <div id="direct-upload-${id}" class="direct-upload direct-upload--pending">
       <div id="direct-upload-progress-${id}" class="direct-upload__progress" style="width: 0%"></div>
@@ -11,26 +11,26 @@ addEventListener('direct-upload:initialize', event => {
 
 addEventListener('direct-upload:start', event => {
   const { id } = event.detail,
-   element = document.getElementById(`direct-upload-${id}`);
+    element = document.getElementById(`direct-upload-${id}`);
   element.classList.remove('direct-upload--pending');
 });
 
 addEventListener('direct-upload:progress', event => {
   const { id, progress } = event.detail,
-   progressElement = document.getElementById(`direct-upload-progress-${id}`);
+    progressElement = document.getElementById(`direct-upload-progress-${id}`);
   progressElement.style.width = `${progress}%`;
 });
 
 addEventListener('direct-upload:error', event => {
   event.preventDefault();
   const { id, error } = event.detail,
-   element = document.getElementById(`direct-upload-${id}`);
+    element = document.getElementById(`direct-upload-${id}`);
   element.classList.add('direct-upload--error');
   element.setAttribute('title', error);
 });
 
 addEventListener('direct-upload:end', event => {
   const { id } = event.detail,
-   element = document.getElementById(`direct-upload-${id}`);
+    element = document.getElementById(`direct-upload-${id}`);
   element.classList.add('direct-upload--complete');
 });
