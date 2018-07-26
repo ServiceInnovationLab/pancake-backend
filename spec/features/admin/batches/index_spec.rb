aca9df43b1499223d19e026aafe1d63d106a986f
Brenda Wallace <shiny@cpan.org>
2018-07-27 11:43:45 +1200

    Specs for batches

---
+++ spec/features/admin/batches/index_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Batch', type: :feature do
+  let!(:batch) { FactoryBot.create :batch }
+  let!(:batch_other_council) { FactoryBot.create :batch }
+
+  context 'anonymous' do
+    it "can't see it" do
+      visit '/admin/batches'
+      expect(page).to have_text('Forgot your password?')
+    end
+  end
+
+  context 'signed in as dia' do
+    let(:user) { FactoryBot.create :admin_user }
+    before { login_as(user, scope: :user) }
+
+    it ' Can see all batches' do
+      visit '/admin/batches'
+      expect(page).to have_text(batch.id)
+      expect(page).to have_text(batch_other_council.id)
+    end
+  end
+
+  context 'signed in as council' do
+    let(:user) { FactoryBot.create :user, council: batch.council }
+    before { login_as(user, scope: :user) }
+
+    it 'ca see batches from my council' do
+      visit '/admin/batches'
+      expect(page).to have_text(batch.id)
+      expect(page).not_to have_text(batch_other_council.id)
+    end
+  end
+end
