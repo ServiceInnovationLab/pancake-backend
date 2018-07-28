228a34a2c1ff96a375b265d7e08baa426ad9884a
Brenda Wallace <shiny@cpan.org>
2018-07-28 17:25:13 +1200

    Set path to wickedpdf in initaliser

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -10,14 +10,7 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
-WickedPdf.config = {
-  # Path to the wkhtmltopdf executable: This usually isn't needed if using
-  # one of the wkhtmltopdf-binary family of gems.
-  # exe_path: '/usr/local/bin/wkhtmltopdf',
-  #   or
-  # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
 
-  # Layout file to be used for all PDFs
-  # (but can be overridden in `render :pdf` calls)
-  # layout: 'pdf.html',
-}
+WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
+
