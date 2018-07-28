5d6db28fbbad4385aaf6545567526755ca87c9b7
Brenda Wallace <shiny@cpan.org>
2018-07-29 11:45:17 +1200

    Removed boilerplate comments from spec file

--- spec/helpers/admin/signatures_helpers_spec.rb
+++ spec/helpers/admin/signatures_helpers_spec.rb
@@ -2,16 +2,6 @@
 
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the Admin::UsersHelper. For example:
-#
-# describe Admin::UsersHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe Admin::SignaturesHelper, type: :helper do
   let(:signature) { FactoryBot.create :signature }
   describe "signature_image_path(signature)" do
