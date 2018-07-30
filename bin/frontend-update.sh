ddb14685fca404a6b2767b8344dbe0d08c81511f
Brenda Wallace <shiny@cpan.org>
2018-07-30 15:29:28 +1200

    Sets configuration by which env we are deploying to

--- bin/frontend-update.sh
+++ bin/frontend-update.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-set -euv
-
+set -eu
 
 APP_NAME="pancake-frontend"
-BRANCH="master"
+BRANCH="feature/config-staging"
 
 git clone https://github.com/ServiceInnovationLab/$APP_NAME.git --branch $BRANCH
 cd $APP_NAME
 
 node --version
 
+echo "Installing npm dependencies"
 npm install
+
+# Set up config file
+if [ $TRAVIS_BRANCH == "master" ]
+then
+  CONFIG_FILE="prod"
+elif [ $TRAVIS_BRANCH == 'dev' ]
+then
+  CONFIG_FILE="staging"
+fi
+
+echo "Setting config to use $CONFIG_FILE"
+
+ln -s "config-${CONFIG_FILE}.js" "src/config.js"
+
 CI=false npm run build
 
 mv build ../public/app
