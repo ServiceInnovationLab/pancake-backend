eb7b8cd650cfb67e0841c03f80e8b388b0c419c4
Brenda Wallace <shiny@cpan.org>
2018-08-02 10:48:06 +1200

    FactoryBot for rebate_form, use existing property record

--- spec/factories/rebate_forms.rb
+++ spec/factories/rebate_forms.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :rebate_form do
     valuation_id { Faker::Vehicle.vin }
-    property { FactoryBot.create :property, valuation_id: valuation_id }
+    property
     # token <-- auto generated. Don't set in factory
     fields("full_name": 'Fred', "income": 0, dependants: 0)
     completed false
@@ -12,6 +12,11 @@ FactoryBot.define do
   end
 
   factory :signed_form, parent: :rebate_form do
+    before(:create) do |rebate_form|
+      unless Property.find_by(valuation_id: rebate_form.valuation_id)
+        FactoryBot.create(:property, valuation_id: rebate_form.valuation_id)
+      end
+    end
     after(:create) do |rebate_form|
       create(:applicant_signature, rebate_form: rebate_form)
       create(:witness_signature, rebate_form: rebate_form)
