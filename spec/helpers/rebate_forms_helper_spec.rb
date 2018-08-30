57dfc2dbf46fe01ffabbb0c591c5a44f74e55f75
Dana Iti <dana.iti.work@gmail.com>
2018-08-31 11:26:17 +1200

    remove comments

--- spec/helpers/rebate_forms_helper_spec.rb
+++ spec/helpers/rebate_forms_helper_spec.rb
@@ -2,22 +2,11 @@
 
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the RebateFormsHelper. For example:
-#
-# describe RebateFormsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
-
 RSpec.describe RebateFormsHelper, type: :helper do
   let(:rebate_form) { FactoryBot.create :rebate_form }
 
   describe 'rebate_form_pdf_dependants(rebate_form)' do
-    it { expect(rebate_form_pdf_dependants(rebate_form)).to eq 'no' }
+    it { expect(rebate_fgitorm_pdf_dependants(rebate_form)).to eq 'no' }
   end
 
   describe 'rebate_form_pdf_home_business(rebate_form)' do
