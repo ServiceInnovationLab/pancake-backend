1d8238168c1ac72a2d38cd750a736d7c1246a0c7
brenda <brenda.wallace@dia.govt.nz>
2018-09-07 18:32:28 +1200

    Include DIA roles when running seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,11 @@ require 'csv'
 SignatureType.find_or_create_by! name: 'applicant'
 SignatureType.find_or_create_by! name: 'witness'
 
+dia_role = Role.find_or_create_by name: 'dia', friendly_name: 'Te Tari Taiwhenua'
+rates_team = Role.find_or_create_by name: 'rates', friendly_name: 'Council rates staff'
+frontline = Role.find_or_create_by name: 'frontline', friendly_name: 'Council frontline staff'
+
 ['brenda.wallace', 'dana.iti'].each do |name|
   email = "#{name}@dia.govt.nz"
-  User.invite! email: email unless User.find_by(email: email)
+  User.invite!(email: email, roles: [dia_role]) unless User.find_by(email: email)
 end
