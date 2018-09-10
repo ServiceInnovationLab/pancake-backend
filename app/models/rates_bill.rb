a913575818696fc2b247e1cba9c61b9586ba0bae
brenda <brenda.wallace@dia.govt.nz>
2018-09-10 14:11:11 +1200

    Auckland rates importer

--- app/models/rates_bill.rb
+++ app/models/rates_bill.rb
@@ -6,6 +6,10 @@ class RatesBill < ApplicationRecord
   validates :total_rates, presence: true
   validates :total_water_rates, presence: true
 
+
+  scope :by_council, -> (council) { joins(:property).where(properties: {council_id: council.id}) }
+  scope :by_rating_year, -> (rating_year) { joins(:property).where(properties: {rating_year: rating_year}) }
+
   def total_bill
     format('%.2f', (total_rates.to_f + total_water_rates.to_f))
   end
