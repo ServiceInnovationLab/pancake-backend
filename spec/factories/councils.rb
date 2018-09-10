473e93e229195af7486d3ec195c7b0e150eb39ce
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-09-11 09:14:05 +1200

    Fixing unique names in factory bot

--- spec/factories/councils.rb
+++ spec/factories/councils.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :council do
     name { "#{Faker::Address.city} City Council" }
-    short_name { "#{Faker::Types.unique.rb_string}#{Faker::Number.number(10)}" }
+    short_name { "#{Faker::Types.rb_string}#{Faker::Number.number(10)}" }
     email { Faker::Internet.email }
     active { true }
   end
