af388346bceadc33f5ecdf590ddd3778e203f611
Masoud Khosravani <khosravani.m@gmail.com>
2018-10-18 17:09:54 +1300

    add test for councils_controller

--- app/controllers/councils_controller.rb
+++ app/controllers/councils_controller.rb
@@ -4,6 +4,7 @@ class CouncilsController < ApiController
   jsonapi resource: CouncilResource
   strong_resource :council
   def show
+    c = Council.all
     scope = jsonapi_scope(Council.where(short_name: params[:id]))
     instance = scope.resolve.first
     raise JsonapiCompliable::Errors::RecordNotFound unless instance
--- spec/controllers/councils_controller_spec.rb
+++ spec/controllers/councils_controller_spec.rb
@@ -3,4 +3,28 @@
 require 'rails_helper'
 
 RSpec.describe CouncilsController, type: :controller do
+
+  describe '#index' do
+    subject { JSON.parse(response.body)['data'] }
+    let!(:council) { FactoryBot.create(:council)}
+    before { get :index, format: :json }
+    it {expect(subject.size).to eq 1}
+    it {expect(subject.first['id']).to eq council.id.to_s}
+  end
+
+  describe '#show' do
+    let!(:council) { FactoryBot.create(:council)}
+    subject { JSON.parse(response.body)['data'] }
+    before { get :show, format: :json, params: {id: council.short_name} }
+    it do
+      is_expected.to eq(
+         "id"=> council.id.to_s,
+         "type"=> "councils",
+         "attributes"=>{"name"=>council.name, "short_name"=>council.short_name, "active"=>council.active}
+      )
+    end
+  end
+
+
+
 end
