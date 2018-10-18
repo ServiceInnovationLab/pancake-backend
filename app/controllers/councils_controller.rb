f847c870558d6cd518d254d1503c94fda1e9a1bd
Masoud Khosravani <khosravani.m@gmail.com>
2018-10-18 17:10:30 +1300

    add test for councils_controller/fix

--- app/controllers/councils_controller.rb
+++ app/controllers/councils_controller.rb
@@ -4,7 +4,6 @@ class CouncilsController < ApiController
   jsonapi resource: CouncilResource
   strong_resource :council
   def show
-    c = Council.all
     scope = jsonapi_scope(Council.where(short_name: params[:id]))
     instance = scope.resolve.first
     raise JsonapiCompliable::Errors::RecordNotFound unless instance
