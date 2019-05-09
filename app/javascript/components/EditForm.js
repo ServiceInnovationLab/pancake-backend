10e889aa412b226311aa1476a4f3d359cb002aec
Mischa Saunders <mischa.saunders@gmail.com>
2019-05-09 12:46:51 +1200

    clean up display

--- app/javascript/components/EditForm.js
+++ app/javascript/components/EditForm.js
@@ -98,9 +98,11 @@ class EditRebateForm extends React.Component {
               <div className="flex-column">
                 <h2 className="flex-item">Income declaration (before tax)</h2>
                 <div className={'flex-row'}>
-                  <label className='flex-item' >Total Combined Income: </label>
+                  <label className='one-third'>
+                    <h3>Total Combined Income: </h3>
+                  </label>
                   <Field
-                    className='rebate-search-input flex-item'
+                    className='rebate-search-input one-third'
                     name="fields.income.total_income"
                     component="input"
                     readOnly
@@ -114,7 +116,9 @@ class EditRebateForm extends React.Component {
                 {map(incomeRows, (field) => {
                 return (
                   <div key={field.id} className="flex-row">
-                      <h3 className="flex-item one-third" >{field.label}</h3>
+                      <label className="flex-item one-third">
+                        <h3>{field.label}</h3>
+                      </label>
                       {TableInput({...field, id: `applicant.${field.id}`})}
                       {TableInput({...field, id: `partner.${field.id}`})}
                   </div>
