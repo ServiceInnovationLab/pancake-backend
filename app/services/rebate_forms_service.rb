d21875444fc9b8190a04a449c89f431996bdc33f
Cara Hill <cara@rabidtech.co.nz>
2019-05-09 14:42:29 +1200

    Updated the rebate forms service to not have so many instance variables and thus be less confusing/brittle.

--- app/services/rebate_forms_service.rb
+++ app/services/rebate_forms_service.rb
@@ -2,16 +2,7 @@
 
 class RebateFormsService
   def initialize(rebate_form_attributes)
-    @id = rebate_form_attributes['id']
-    @valuation_id = rebate_form_attributes['valuation_id']
-    @create_fields = rebate_form_attributes['fields']
-    @update_fields = if rebate_form_attributes['rebate_form']
-                       rebate_form_attributes['rebate_form']['fields']
-                     else
-                       @create_fields
-                     end
-    @location = rebate_form_attributes['location']
-    @total_rates = rebate_form_attributes['total_rates']
+    @rebate_form_attributes = rebate_form_attributes
   end
 
   def update
@@ -26,38 +17,43 @@ class RebateFormsService
   private
 
   def create_or_update_rebate_form(property)
-    return update_rebate_form(property) if @id
+    return update_rebate_form(property) if @rebate_form_attributes['id']
     create_rebate_form(property)
   end
 
   def update_rebate_form(property)
-    rebate_form = RebateForm.find_by(id: @id)
+    rebate_form = RebateForm.find_by(id: @rebate_form_attributes['id'])
     property = rebate_form.property if property.id.nil?
-    fields_to_update = rebate_form.fields.merge(merged_fields)
+    fields_to_update = rebate_form.fields.merge(fields_to_merge)
     rebate_form.update(property: property, valuation_id: property.valuation_id, fields: fields_to_update)
     rebate_form
   end
 
-  def merged_fields
-    return @update_fields unless @update_fields.nil?
+  def update_fields
+    return @rebate_form_attributes['rebate_form']['fields'] if @rebate_form_attributes['rebate_form']
+    @rebate_form_attributes['fields']
+  end
+
+  def fields_to_merge
+    return update_fields unless update_fields.nil?
     {}
   end
 
   def create_rebate_form(property)
     RebateForm.create(property: property,
                       valuation_id: property.valuation_id,
-                      fields: @create_fields)
+                      fields: @rebate_form_attributes['fields'])
   end
 
   def create_or_update_property
-    Property.find_or_create_by(valuation_id: @valuation_id,
-                               location: @location,
+    Property.find_or_create_by(valuation_id: @rebate_form_attributes['valuation_id'],
+                               location: @rebate_form_attributes['location'],
                                rating_year: ENV['YEAR'])
   end
 
   def update_rates_bill(property)
     RatesBill.find_or_create_by(property: property,
                                 rating_year: property.rating_year,
-                                total_rates: @total_rates)
+                                total_rates: @rebate_form_attributes['total_rates'])
   end
 end
