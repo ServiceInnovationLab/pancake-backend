f301b655257bd03aa22fc34e91aa1a4427800ea4
Mischa Saunders <mischa.saunders@gmail.com>
2019-05-13 10:34:05 +1200

    remove labels for deploy

--- app/javascript/components/inputs.js
+++ app/javascript/components/inputs.js
@@ -61,9 +61,8 @@ export function FieldArrayInput () {
       {({ fields }) => 
         fields.map((name, index) => (
           <div key={name} className="flex-row">
-            {ExtraTableInputs({classname: 'flex-item', name: `${name}.${index}`, type: 'string'})}
-            {ExtraTableInputs({classname: 'flex-item', name: `${name}.applicant-${index}`})}
-            {ExtraTableInputs({classname: 'flex-item', name: `${name}.partner-${index}`})}
+            {ExtraTableInputs({classname: 'flex-item', name: `${name}other-${index}-applicant`})}
+            {ExtraTableInputs({classname: 'flex-item', name: `${name}other-${index}-partner`})}
             <span
               onClick={() => fields.remove(index)}
               style={{ cursor: 'pointer' }}
