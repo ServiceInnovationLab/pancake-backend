cee94f8da2d72e04f7b6cbaa990dc78a6e0bd314
Juan van den Anker <juan@vdanker.net>
2019-05-13 13:41:35 +1200

    AWS configuration and modified seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ dia_role = Role.find_or_create_by name: 'dia', friendly_name: 'Te Tari Taiwhenua
 rates_team = Role.find_or_create_by name: 'rates', friendly_name: 'Council rates staff'
 frontline = Role.find_or_create_by name: 'frontline', friendly_name: 'Council frontline staff'
 
-['brenda.wallace', 'dana.iti'].each do |name|
-  email = "#{name}@dia.govt.nz"
+['juanvandenanker@gmail.com'].each do |email|
+  #email = "#{name}@dia.govt.nz"
   User.invite!(email: email, roles: [dia_role]) unless User.find_by(email: email)
 end
