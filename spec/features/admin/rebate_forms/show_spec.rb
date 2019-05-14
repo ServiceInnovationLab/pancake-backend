084d31b5a0575202253bb2c1b94d316f5a5165fe
Cara Hill <cara@rabidtech.co.nz>
2019-05-14 13:53:18 +1200

    Added tests to ensure header buttons go to the right places.

--- spec/features/admin/rebate_forms/show_spec.rb
+++ spec/features/admin/rebate_forms/show_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'RebateForm', type: :feature do
 
     before { login_as(user, scope: :user) }
 
-    context 'rebate form is not completed' do
+    context 'when the rebate form is not completed' do
       it ' Can see customer details' do
         visit "/admin/rebate_forms/#{rebate_form.id}"
         expect(page).to have_text('Customer details')
@@ -19,9 +19,33 @@ RSpec.describe 'RebateForm', type: :feature do
         expect(page).to have_text(rebate_form.fields['email'])
         expect(page).to have_text(rebate_form.fields['occupation'])
       end
+
+      context 'when the back button is clicked' do
+        it 'goes to the right place' do
+          visit "/admin/rebate_forms/#{rebate_form.id}"
+          click_link('back')
+          expect(page).to have_text('Rates Rebate 2018/2019')
+        end
+      end
+
+      context 'when the reload button is clicked' do
+        it 'goes to the right place' do
+          visit "/admin/rebate_forms/#{rebate_form.id}"
+          click_link('reload')
+          expect(page).to have_http_status :ok
+        end
+      end
+
+      context 'when the edit button is clicked' do
+        it 'goes to the right place' do
+          visit "/admin/rebate_forms/#{rebate_form.id}"
+          click_link('edit')
+          expect(page).to_not have_text('EDIT')
+        end
+      end
     end
 
-    context 'rebate form is completed' do
+    context 'when the rebate form is completed' do
       before { rebate_form.update(completed: true) }
 
       it ' Can see customer details' do
