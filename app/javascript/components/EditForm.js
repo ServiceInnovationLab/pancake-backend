3bda9ff52c66a1b68a2c88779185e13f319f1b0b
Mischa Saunders <mischa.saunders@gmail.com>
2019-05-17 15:14:04 +1200

    change reference to app url to use window instead of env

--- app/javascript/components/EditForm.js
+++ app/javascript/components/EditForm.js
@@ -11,7 +11,7 @@ import { calculator } from '../helpers/calculator_decorator';
 import { SingleInput, RadioInput } from './inputs'
 import { IncomeDeclaration } from "./IncomeDeclaration";
 
-const appUrl = process.env.APP_URL  
+const appUrl = window.location.origin  
 
 class EditRebateForm extends React.Component {
 
@@ -38,7 +38,7 @@ class EditRebateForm extends React.Component {
   onSubmit (values) {
     return values.newIncomeField
       ? this.addNewIncomeValue(values)
-      : fetch(`${appUrl}admin/rebate_forms/${this.props.rebateForm.id}`, {
+      : fetch(`${appUrl}/admin/rebate_forms/${this.props.rebateForm.id}`, {
         method: 'PATCH',
         headers: {
           'X-CSRF-Token': getCSRF(),
