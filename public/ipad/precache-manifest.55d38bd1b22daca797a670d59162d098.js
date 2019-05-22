21672a97b5eeb8256fc4aa5eb436a681cbb3a3f1
Mischa Saunders <mischa.saunders@gmail.com>
2019-05-22 14:25:56 +1200

    add latest ipad changes

---
+++ public/ipad/precache-manifest.55d38bd1b22daca797a670d59162d098.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c812c15f875c071daf4ce89e7503e948",
+    "url": "/ipad/index.html"
+  },
+  {
+    "revision": "fd5524ecbbf103aba3f6",
+    "url": "/ipad/static/css/main.16942e9c.chunk.css"
+  },
+  {
+    "revision": "7d3f52c032539c3d9ec8",
+    "url": "/ipad/static/js/2.3c1a2099.chunk.js"
+  },
+  {
+    "revision": "fd5524ecbbf103aba3f6",
+    "url": "/ipad/static/js/main.c8c9dfe1.chunk.js"
+  },
+  {
+    "revision": "dd0051aba38b01f68560",
+    "url": "/ipad/static/js/runtime~main.8b4588fc.js"
+  },
+  {
+    "revision": "25037ef8d155e38b5df0c242a4c6cf2d",
+    "url": "/ipad/static/media/FiraSans-Bold.25037ef8.ttf"
+  },
+  {
+    "revision": "494219a9639084eb9528ff47f79fcda1",
+    "url": "/ipad/static/media/FiraSans-Bold.494219a9.woff"
+  },
+  {
+    "revision": "5b6db7a387815eea8bec2d491683a707",
+    "url": "/ipad/static/media/FiraSans-Bold.5b6db7a3.eot"
+  },
+  {
+    "revision": "12801b91616871ee8783342abc4cd110",
+    "url": "/ipad/static/media/FiraSans-Regular.12801b91.eot"
+  },
+  {
+    "revision": "537188a19aeebdd74a92e114af7a02cb",
+    "url": "/ipad/static/media/FiraSans-Regular.537188a1.ttf"
+  },
+  {
+    "revision": "5d6a923de9be80ff5c2995cc03d93127",
+    "url": "/ipad/static/media/FiraSans-Regular.5d6a923d.woff"
+  },
+  {
+    "revision": "bd37d876a5f4d0996777a69c50815803",
+    "url": "/ipad/static/media/tick.bd37d876.svg"
+  }
+]);
