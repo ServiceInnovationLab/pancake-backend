2286bfdfd2a8d458ed0071b23992b5f70b107dba
Cara Hill <cara@rabidtech.co.nz>
2019-05-23 16:56:31 +1200

    REmoved unneeded code, started update of mailer content.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  #default from: 'noreply@dia.govt.nz'
   default from: 'info@digital.govt.nz'
   layout 'mailer'
 end
--- app/mailers/rebate_forms_mailer.rb
+++ app/mailers/rebate_forms_mailer.rb
@@ -3,8 +3,7 @@
 class RebateFormsMailer < ApplicationMailer
   def council_mail
     @rebate_form = params[:rebate_form]
-    #council_mail = @rebate_form.council.email
-    council_mail = 'juanvandenanker@gmail.com'
+    council_mail = @rebate_form.council.email
     subject = "Rates rebate application for #{@rebate_form.property.location}"
     mail(to: council_mail, subject: subject)
   end
@@ -12,8 +11,7 @@ class RebateFormsMailer < ApplicationMailer
   def applicant_mail
     @rebate_form = params[:rebate_form]
     @council = @rebate_form.council
-    #applicant_email = @rebate_form.email
-    applicant_email = 'juanvandenanker@gmail.com'
+    applicant_email = @rebate_form.email
     subject = 'Your Rates Rebate application'
     mail(to: applicant_email, subject: subject)
   end
