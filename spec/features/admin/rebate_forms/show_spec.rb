65522c649b2a1de99fd27df67988ff4e293e2041
Brenda <brenda.wallace@dia.govt.nz>
2019-05-24 19:45:13 +1200

    Check for qr

--- spec/features/admin/rebate_forms/show_spec.rb
+++ spec/features/admin/rebate_forms/show_spec.rb
@@ -79,16 +79,17 @@ RSpec.describe 'RebateForm', type: :feature, js: true do
       context 'when the generate QR button is clicked' do
         describe 'requests the QR code' do
           before do
-            Timecop.freeze(Time.local.utc(2019, 9, 1, 10, 5, 0))
+            # Timecop.freeze(Time.local(2019, 9, 1, 10, 5, 0))
             visit "/admin/rebate_forms/#{rebate_form.id}/"
             # Freeze time when we generate the qr
             find("img[class='rebate-generate-qr']").click
           end
-          it { expect(page).to_not have_text('EDIT') }
-          it { expect(page).to_not have_text('Customer details') }
-          it { expect(page).to_not have_text('Signature required') }
+          it "displays the qr code" do
+            expect(page).to have_image(class: 'rebate-qrcode')
+            expect(page).to have_link 'DONE'
+          end
           include_examples 'percy snapshot'
-          after { Timecop.return }
+          # after { Timecop.return }
         end
       end
     end
