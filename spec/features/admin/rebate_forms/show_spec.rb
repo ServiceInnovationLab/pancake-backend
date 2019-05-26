10130fb984fb0b67771cf0069f14221fcf426394
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-05-27 09:32:05 +1200

    Time.local -> Time.zone.local

--- spec/features/admin/rebate_forms/show_spec.rb
+++ spec/features/admin/rebate_forms/show_spec.rb
@@ -79,7 +79,7 @@ RSpec.describe 'RebateForm', type: :feature, js: true do
       context 'when the generate QR button is clicked' do
         describe 'requests the QR code' do
           before do
-            Timecop.freeze(Time.local(2019, 9, 1, 10, 5, 0))
+            Timecop.freeze(Time.zone.local(2019, 9, 1, 10, 5, 0))
             visit "/admin/rebate_forms/#{rebate_form.id}/"
             # Freeze time when we generate the qr
             find("img[class='rebate-generate-qr']").click
