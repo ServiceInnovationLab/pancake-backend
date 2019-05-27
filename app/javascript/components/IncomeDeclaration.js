748bd2005097fc93097f40c126502447b58f3553
Mischa Saunders <mischa.saunders@gmail.com>
2019-05-27 16:37:06 +1200

    Fix/rr366 (#316) Refactor API requests to use functions prepare data for/from the API
    * add property location to rebate fields on filing of new rebate form
    
    * fix other partner values
    
    * remove redundant code
    
    * add tests
    
    * remove location from fields
    
    * fix reload edit page test
    
    * remove unused variable
    
    * cleanup eslint and lint
    
    * update tests
    
    * fix final test

--- app/javascript/components/IncomeDeclaration.js
+++ app/javascript/components/IncomeDeclaration.js
@@ -1,9 +1,9 @@
-import React from "react"
-import { Field } from "react-final-form";
-import { map } from "lodash"
+import React from 'react';
+import { Field } from 'react-final-form';
+import { map } from 'lodash';
 
-import { incomeRows } from '../helpers/data'
-import { TableInput } from './inputs'
+import { incomeRows } from '../helpers/data';
+import { TableInput } from './inputs';
 
 export function IncomeDeclaration ({otherIncomeFields, isReadOnly, includePartnerValues}) {
   return (
@@ -22,33 +22,33 @@ export function IncomeDeclaration ({otherIncomeFields, isReadOnly, includePartne
           />
         </div>
         <div className="flex-row">
-            <h2 className="flex-item" >Income Type</h2>
-            <h2 className="one-quarter" >Applicant</h2>
-            {includePartnerValues && <h2 className="one-quarter" >Partner or joint homeowner</h2>}
+          <h2 className="flex-item" >Income Type</h2>
+          <h2 className="one-quarter" >Applicant</h2>
+          {includePartnerValues && <h2 className="one-quarter" >Partner or joint homeowner</h2>}
         </div>
         {map(incomeRows, ([name, label]) => {
           return (
             <div key={name} className="flex-row">
-                <label className="flex-item">
-                  <h3>{label || name}</h3>
-                </label>
-                {TableInput({isReadOnly, className: 'one-quarter with-margin', id: `applicant.${name}`})}
-                {includePartnerValues && TableInput({isReadOnly, className: 'one-quarter', id: `partner.${name}`})}
+              <label className="flex-item">
+                <h3>{label || name}</h3>
+              </label>
+              {TableInput({isReadOnly, className: 'one-quarter with-margin', id: `applicant.${name}`})}
+              {includePartnerValues && TableInput({isReadOnly, className: 'one-quarter', id: `partner.${name}`})}
             </div>
-          )}
+          );}
         )}
         {map(otherIncomeFields, (name) => {
           return (
             <div key={name} className="flex-row">
-                <label className="flex-item">
-                  <h3>{name}</h3>
-                </label>
-                {TableInput({isReadOnly, className: 'one-quarter with-margin', id: `otherIncome.applicant.${name}`})}
-                {includePartnerValues && TableInput({isReadOnly, className: 'one-quarter', id: `otherIncome.partner.${name}`})}
+              <label className="flex-item">
+                <h3>{name}</h3>
+              </label>
+              {TableInput({isReadOnly, className: 'one-quarter with-margin', id: `otherIncome.applicant.${name}`})}
+              {includePartnerValues && TableInput({isReadOnly, className: 'one-quarter', id: `otherIncome.partner.${name}`})}
             </div>
-          )}
+          );}
         )}
       </div>
     </div>
-  )
+  );
 }
