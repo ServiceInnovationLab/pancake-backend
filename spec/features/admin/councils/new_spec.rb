26cf6f03c57a652de9b56cc507c969368fa8f619
Jacob <jacob.sien.ong@gmail.com>
2019-05-27 14:07:50 +1200

    Added some new lines of test.

--- spec/features/admin/councils/new_spec.rb
+++ spec/features/admin/councils/new_spec.rb
@@ -13,26 +13,31 @@ RSpec.describe 'Council', type: :feature, js: true do
     end
   end
 
-  context 'can edit' do
+  context 'edit form is displayed' do
     let(:user) { FactoryBot.create :admin_user }
     before do
       login_as(user, scope: :user)
       visit '/admin/councils/new'
     end
+
     describe 'can create new council' do
       it { expect(page).to have_text('New council') }
       it { expect(page).to have_text('Name') }
       it { expect(page).to have_field('council_name') }
+      it { fill_in 'Name', with: 'New Council 1' }
       it { expect(page).to have_text('This is a required field.') }
       it { expect(page).to have_text('Short name') }
       it { expect(page).to have_field('council_short_name') }
+      it { fill_in 'Short name', with: 'New C1' }
       it { expect(page).to have_text('This is a required field.') }
       it { expect(page).to have_text('Email') }
       it { expect(page).to have_field('council_email') }
+      it { fill_in 'Email', with: 'C1@council.co.nz' }
       it { expect(page).to have_text('where to send') }
       it { expect(page).to have_unchecked_field('council_active') }
       it { expect(page).to have_text('Active') }
       it { expect(page).to have_selector(:link_or_button, 'Save') }
+      it { click_button 'Save' }
       it { expect(page).to have_selector(:link_or_button, 'Back') }
     end
   end
