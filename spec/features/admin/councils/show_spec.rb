0822bd4e840670b4dd8a1002c3c5a23ceac6fb47
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-05-28 12:14:18 +1200

    Add percy snapshots to council admin

--- spec/features/admin/councils/show_spec.rb
+++ spec/features/admin/councils/show_spec.rb
@@ -10,8 +10,10 @@ RSpec.describe 'Council', type: :feature do
   before do
     # Set up some data
     3.times do
-      FactoryBot.create(:rebate_form, completed: true, property: FactoryBot.create(:property_with_rates, council: council))
-      FactoryBot.create(:rebate_form, completed: false, property: FactoryBot.create(:property_with_rates, council: council))
+      FactoryBot.create(:rebate_form, completed: true,
+                                      property: FactoryBot.create(:property_with_rates, council: council))
+      FactoryBot.create(:rebate_form, completed: false,
+                                      property: FactoryBot.create(:property_with_rates, council: council))
     end
   end
 
@@ -33,9 +35,10 @@ RSpec.describe 'Council', type: :feature do
     let(:user) { FactoryBot.create :user, council: council }
 
     it { is_expected.to have_http_status :ok }
-    it { is_expected.to have_text(council.name) }
-    it { expect(council.rebate_forms.size).to eq 6 }
-    it { expect(council.properties.size).to eq 6 }
+    describe 'show name of council' do
+      it { expect(page).to have_text(council.name) }
+      include_examples 'percy snapshot'
+    end
     it { is_expected.to have_text('$555.12') } # average
     it { is_expected.to have_text('$3,330.72') } # sum
   end
