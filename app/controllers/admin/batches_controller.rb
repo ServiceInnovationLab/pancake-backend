7fcada48b97a63de3a34a1d34c2188e23747bd17
Cara Hill <cara@rabidtech.co.nz>
2019-05-30 16:16:35 +1200

    Added name field to batch, added after_create to batch to add a temp name to all new batches, updated create action in batch controller, cleaned up property factory, updated show header so that edit button does not appear for processed rebate forms.

--- app/controllers/admin/batches_controller.rb
+++ app/controllers/admin/batches_controller.rb
@@ -25,33 +25,22 @@ class Admin::BatchesController < Admin::BaseController
   end
 
   def create
-    @council = current_user.council
+    rebate_forms = params[:ids].map { |id| policy_scope(RebateForm).find(id) }.order(created_at: :desc)
 
-    @rebate_forms = policy_scope(RebateForm)
-                    .joins(:property)
-                    .where(status: RebateForm::SIGNED_STATUS,
-                           properties: { council: @council },
-                           batch: nil)
-                    .order(:created_at)
-                    .limit(100)
+    batch = Batch.new(
+      council: current_user.council,
+      amount: rebate_forms.sum(:rebate),
+      batch_date: rebate_forms.last.created_at,
+      claim_count: rebate_forms.size
+    )
+    authorize batch
 
-    if @rebate_forms.size.positive?
-      @batch = Batch.new(
-        council: current_user.council,
-        amount: @rebate_forms.sum(:rebate),
-        batch_date: @rebate_forms.last.created_at,
-        claim_count: @rebate_forms.size
-      )
-      authorize @batch
-
-      Batch.transaction do
-        @batch.save
-        @rebate_forms.each do |form|
-          form.update(batch: @batch)
-        end
+    Batch.transaction do
+      batch.save
+      rebate_forms.each do |form|
+        form.transition_to_batched_state(batch)
       end
     end
-    redirect_to admin_batches_path
   end
 
   private
--- spec/factories/properties.rb
+++ spec/factories/properties.rb
@@ -3,12 +3,13 @@
 FactoryBot.define do
   factory :property do
     valuation_id { Faker::Vehicle.vin }
-    location { '999 Lambton quay' }
-    suburb { 'coolville' }
-    town_city { 'Tauranga' }
+    location { Faker::Address.street_address }
+    suburb { Faker::Address.community }
+    town_city { Faker::Address.city }
     council
     rating_year { Rails.configuration.rating_year }
   end
+
   factory :property_with_rates, parent: :property do
     after(:create) do |property|
       # create :rates_payer, property: property, rating_year: property.rating_year
