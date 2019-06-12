36f3c9de4390d8927ef1adeb0fc5d7bfad176adb
Mischa Saunders <mischa.saunders@gmail.com>
2019-06-12 16:30:15 +1200

    remove route_to test covered by redirect test

--- spec/routing/admin/rebate_forms_routing_spec.rb
+++ spec/routing/admin/rebate_forms_routing_spec.rb
@@ -5,10 +5,6 @@ require 'rails_helper'
 RSpec.describe Admin::RebateFormsController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/').to route_to('admin/rebate_forms#index')
-    end
-
-    it 'routes to #index' do
       expect(get: '/admin').to route_to('admin/rebate_forms#index')
     end
 
