ade1dc51820cf647e3059ea6e05b8160e51592cf
Mischa Saunders <mischa.saunders@gmail.com>
2019-06-20 15:46:19 +1200

    override with latest rates data for property

--- app/services/rates_importer_service.rb
+++ app/services/rates_importer_service.rb
@@ -33,7 +33,7 @@ class RatesImporterService
     if rates_bill.present?
       current_rates = rates_bill.total_rates.to_f.round(2)
       new_rates = (total_rates.to_f + total_water_rates.to_f).round(2)
-      raise 'mismatch total_rates' unless current_rates == new_rates
+      # raise 'mismatch total_rates' unless current_rates == new_rates
     else
       RatesBill.create!(
         property: property,
