95c8929f571deed2fd28d2dbb23cb018f963981b
Mischa Saunders <mischa.saunders@gmail.com>
2019-06-24 11:26:17 +1200

    include update up

--- db/migrate/20190620204258_add_total_rates_location_to_rebate_form.rb
+++ db/migrate/20190620204258_add_total_rates_location_to_rebate_form.rb
@@ -9,4 +9,14 @@ class AddTotalRatesLocationToRebateForm < ActiveRecord::Migration[5.2]
     remove_column :rebate_forms, :location, :string
     remove_column :rebate_forms, :total_rates, :decimal, precision: 8, scale: 2
   end
+
+  private
+
+def update_existing_rebate_forms_up
+  RebateForm.all.each do |rebate_form|
+    property = rebate_form.property
+    rebate_form.update(location: property.location,
+                       total_rates: property.rates_bills[0].total_rates,
+                       valuation_id: rebate_form.fields['valuation_id'] || property.valuation_id || nil)
+  end
 end
