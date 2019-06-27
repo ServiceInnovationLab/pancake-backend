759725b5808c5dda5ac9e71b15926135df6bc978
Cara Hill <cara@rabidtech.co.nz>
2019-06-27 16:01:42 +1200

    Deleted the content for the api/v1/signatures/create_spec until the work of transferring over the SignRebateFormsController happens. Got the api/v1/rebate_forms/create_spec tests passing.

--- spec/api/v1/signatures/create_spec.rb
+++ spec/api/v1/signatures/create_spec.rb
@@ -3,29 +3,4 @@
 require 'rails_helper'
 
 RSpec.describe 'signatures#create', type: :request do
-  subject(:make_request) do
-    jsonapi_post '/api/v1/signatures', payload
-  end
-
-  describe 'basic create' do
-    let(:payload) do
-      {
-        data: {
-          type: 'signatures',
-          attributes: {
-            # ... your attrs here
-          }
-        }
-      }
-    end
-
-    pending 'creates the resource' do
-      expect do
-        make_request
-      end.to change(Signature, :count).by(1)
-      signature = Signature.last
-
-      assert_payload(:signature, signature, json_item)
-    end
-  end
 end
