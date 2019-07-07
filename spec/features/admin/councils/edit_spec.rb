7720d5ff4fb8b01de2783948b6ddbfbc3b8d2bb1
Mischa Saunders <mischa.saunders@gmail.com>
2019-07-08 10:59:41 +1200

    remove future and beyond

--- spec/features/admin/councils/edit_spec.rb
+++ spec/features/admin/councils/edit_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Council', type: :feature, js: true do
   context 'anonymous' do
     it "can't see it" do
       visit "/admin/councils/#{council.id}"
-      expect(page).to have_text('Rates Rebate - The Future and Beyond')
+      expect(page).to have_text('Rates Rebate')
       expect(page).to have_text('Log in')
     end
   end
