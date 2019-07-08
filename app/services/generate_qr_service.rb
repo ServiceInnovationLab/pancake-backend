1705d8e487b55d0b4178bee9517fa80fe00b1535
Lyall Morrison <lyall@rabidtech.co.nz>
2019-07-08 14:28:09 +1200

    refactor for easier testing

--- app/services/generate_qr_service.rb
+++ app/services/generate_qr_service.rb
@@ -10,12 +10,8 @@ class GenerateQrService
     # create a new token that only allows you to fetch a restricted set of details for this application only
     # and use the same token to submit the signatures back
     # this token is valid for 30 minutes
+    url = signing_url
 
-    payload = payload_details
-
-    token = JWT.encode payload, ENV['HMAC_SECRET'], 'HS256'
-    # iPad-application URL
-    url = ENV['APP_URL'] + 'ipad/?t=' + token
     Rails.logger.info('the ipad signing url is:' + url) # for easier debugging in production
     qr_code(url)
   end
@@ -33,7 +29,7 @@ class GenerateQrService
 
   def witness_details(current_user)
     {
-      name: current_user.name || '',
+      name: current_user&.name || '',
       location: current_user&.council&.name,
       occupation: 'Authorised Council Officer'
     }
@@ -44,5 +40,12 @@ class GenerateQrService
       .new(url, size: 20, level: :h)
       .as_png(offset: 0, color: '0', shape_rendering: 'crispEdges', module_size: 10)
       .to_data_url
+  end
+
+  def signing_url
+    token = JWT.encode payload_details, ENV['HMAC_SECRET'], 'HS256'
+
+    # iPad-application URL
+    "#{ENV['APP_URL']}ipad/?t=#{token}"
   end
 end
