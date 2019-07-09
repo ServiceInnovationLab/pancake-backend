f37b49c38ae1727a86b86e55b6cee3290313d26e
Lyall Morrison <lyall@rabidtech.co.nz>
2019-07-09 12:38:09 +1200

    Load db seed before tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -88,6 +88,10 @@ RSpec.configure do |config|
 
   config.before(:each) do
     DatabaseCleaner.start
+    # Load database seeds before tests
+    #
+    # This is required for some static data like SignatureTypes in some tests
+    Rails.application.load_seed
   end
 
   config.append_after(:each) do
